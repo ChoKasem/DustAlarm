bd8c7635817a9f8cbbe63fc451fd9126e72e902e
Chokpisit Kasemphaibulsuk <chokpisit.kasemphaibulsuk@mail.utoronto.ca>
2021-02-20 22:43:37 +0700

    send bluetooth data to laptop

--- master3/master3.ino
+++ master3/master3.ino
@@ -1,4 +1,4 @@
-#include <Wire.h> 
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <SoftwareSerial.h>
 
@@ -23,65 +23,69 @@ int RxD = 5;
 
 SoftwareSerial mySerial(RxD, TxD);
 
-void setup(){
+void setup() {
   Serial.begin(9600);
   mySerial.begin(9600);
   lcd.begin();
-  pinMode(ledPower,OUTPUT);
+  pinMode(ledPower, OUTPUT);
   pinMode(buzzer, OUTPUT);
   lcd.backlight();
 }
 
-void loop(){
-  digitalWrite(ledPower,LOW);
+void loop() {
+  digitalWrite(ledPower, LOW);
   delayMicroseconds(samplingTime);
   refValue = analogRead(POT);
   voMeasured = analogRead(measurePin);
 
   delayMicroseconds(deltaTime);
-  digitalWrite(ledPower,HIGH);
+  digitalWrite(ledPower, HIGH);
   delayMicroseconds(sleepTime);
 
-  calcVoltage = voMeasured*(5.0/1024);
-  dustDensity = (0.17*calcVoltage-0.1)*1000;
+  calcVoltage = voMeasured * (5.0 / 1024);
+  dustDensity = (0.17 * calcVoltage - 0.1) * 1000;
 
   if ( dustDensity < 0)
   {
     dustDensity = 0.00;
   }
-//
-//  Serial.println("Raw Signal Value (0-1023):");
-//  Serial.println(voMeasured);
+  //
+  //  Serial.println("Raw Signal Value (0-1023):");
+  //  Serial.println(voMeasured);
 
-//  Serial.println("Voltage:");
-//  Serial.println(calcVoltage);
-//
-//  Serial.println("Dust Density:");
+  //  Serial.println("Voltage:");
+  //  Serial.println(calcVoltage);
+  //
+  //  Serial.println("Dust Density:");
   Serial.println(dustDensity);
   print_lcd(dustDensity, refValue, lcd);
-  if(dustDensity > refValue){
+  if (dustDensity > refValue) {
     tone(buzzer, 1000); // Send 1KHz sound signal...
-//    delay(1000);        // ...for 1 sec
-//    noTone(buzzer);     // Stop sound...
-//    delay(1000);
+    if (mySerial.available()) {
+      mySerial.println('1');
+    }
+    //    delay(1000);        // ...for 1 sec
+    //    noTone(buzzer);     // Stop sound...
+    //    delay(1000);
   }
-  else{
+  else {
     noTone(buzzer);
+    if (mySerial.available()) {
+      mySerial.println('0');
+    }
   }
-  if(mySerial.available()){
-    mySerial.println(dustDensity);
-  }
+
   delay(750);
 }
 
 void print_lcd(float curr, float ref, LiquidCrystal_I2C lcd)
 {
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   String refstr = String(ref);
   String currstr = String(curr);
   lcd.print("DustLv:");
   lcd.print(currstr);
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("Ref(ug/m3):");
   lcd.print(refstr);
 }
